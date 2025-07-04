fdbe9f4411e0d02625346e72e4cba937332d876e
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-07-04 22:57:16 +0530

    Refactor Node class to include attributes and constructor

--- Node.java
+++ Node.java
@@ -1,3 +1,17 @@
-public class Node {
+import java.util.ArrayDeque;
+import java.util.Queue;
 
+public class Node {
+    // Attributes
+    private static Queue<Integer> q;
+    private int pid; // process id
+    private Node next; // pointer to next node
+
+    // Constructor
+    public Node(int pid) {
+        this.pid = pid;
+        q = new ArrayDeque<>();
+    }
+
+    // Methods
 }
