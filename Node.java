7ebc8efc2f79b8c66a106cf4df68ad3e881c24b5
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-07-04 23:15:18 +0530

    Refactor Node class to implement Runnable and enhance token management

--- Node.java
+++ Node.java
@@ -1,20 +1,38 @@
+import java.io.IOException;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
-public class Node {
+public class Node implements Runnable {
     // Attributes
-    private static Queue<Integer> q;
+    public static Queue<Integer> q;
     private int pid; // process id
     private Node next; // pointer to next node
+    private boolean hasToken;
+    private boolean wantToEnter;
 
     // Constructor
     public Node(int pid) {
         this.pid = pid;
         q = new ArrayDeque<>();
+        this.hasToken = false;
+        this.wantToEnter = false;
     }
 
     // Methods
     public void setNext(Node x) {
         this.next = x;
+    }
+
+    public void setToken(boolean b) {
+        this.hasToken = true;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep((long) (Math.random() * 20 + 1));
+        } catch (Exception e) {
+            System.out.println("Exception occured: " + e.getMessage());
+        }
     }
 }
